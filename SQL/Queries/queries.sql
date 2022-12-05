a05427213465b8bbe2da14ee1c1e1ebe5c03ce69
Abdirahmanroble <56278629+Abdirahmanroble@users.noreply.github.com>
2022-12-05 22:53:24 +0100

    updated

--- SQL/Queries/queries.sql
+++ SQL/Queries/queries.sql
@@ -1,11 +1,11 @@
 -- all lessons taken in 2022
-   EXPLAIN SELECT
+  CREATE VIEW SELECT
     EXTRACT(month FROM start_time) AS month,
     count(*) FROM lesson WHERE EXTRACT(YEAR FROM start_time) = '2022' GROUP BY EXTRACT(month FROM start_time)
     ORDER BY EXTRACT(month FROM start_time) ASC;
 
 -- all private lessons taken in 2022
-SELECT
+ CREATE VIEW SELECT
     EXTRACT(month FROM start_time) AS month,
     count(*) FROM lesson 
 	RIGHT JOIN private_lesson ON  lesson.id = private_lesson.lesson_id 
@@ -14,7 +14,7 @@ SELECT
 
 
 -- all group lessons taken in 2022
-SELECT
+ CREATE VIEW SELECT
     EXTRACT(month FROM start_time) AS month,
     count(*) FROM lesson 
 	RIGHT JOIN group_lesson ON  lesson.id = group_lesson.lesson_id 
@@ -24,7 +24,7 @@ SELECT
 
 
 -- all ensembles taken in 2022
-SELECT
+ CREATE VIEW SELECT
     EXTRACT(month FROM start_time) AS month,
     count(*) FROM lesson 
 	RIGHT JOIN ensembles ON  lesson.id = ensembles.lesson_id 
@@ -35,17 +35,17 @@ SELECT
 
 -- siblings
 
-  SELECT student.person_id, COUNT(CASE WHEN sibling.student_id IS NOT NULL THEN 'has_sibling' END) AS number_of_siblings from student LEFT JOIN sibling ON student.person_id = sibling.student_id GROUP BY student.person_id ORDER BY student.person_id;
+  CREATE VIEW SELECT student.person_id, COUNT(CASE WHEN sibling.student_id IS NOT NULL THEN 'has_sibling' END) AS number_of_siblings from student LEFT JOIN sibling ON student.person_id = sibling.student_id GROUP BY student.person_id ORDER BY student.person_id;
 
 
 
  -- instructor lessons
 
-  SELECT instructor_person_id, count(*) FROM lesson WHERE EXTRACT(YEAR FROM start_time) = '2022' AND EXTRACT(MONTH FROM start_time) = '12' GROUP BY instructor_person_id HAVING COUNT(*) > 0
+ CREATE VIEW SELECT instructor_person_id, count(*) FROM lesson WHERE EXTRACT(YEAR FROM start_time) = '2022' AND EXTRACT(MONTH FROM start_time) = '12' GROUP BY instructor_person_id HAVING COUNT(*) > 0
     ORDER BY count(*) DESC;
 
 -- Show all ensembles in next week
-   SELECT lesson_id, to_char(start_time, 'Day') as weekday, genre, start_time,
+  CREATE VIEW  SELECT lesson_id, to_char(start_time, 'Day') as weekday, genre, start_time,
    CASE 
     WHEN  (max_of_students - 10) = 0 
     THEN 'full boked'
