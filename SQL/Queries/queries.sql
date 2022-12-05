27a377376fb8db9f509caec580ec86ede142eed0
Abdirahmanroble <56278629+Abdirahmanroble@users.noreply.github.com>
2022-12-05 21:37:57 +0100

    Update queries.sql

--- SQL/Queries/queries.sql
+++ SQL/Queries/queries.sql
@@ -1,5 +1,5 @@
 -- all lessons taken in 2022
-  SELECT
+   EXPLAIN SELECT
     EXTRACT(month FROM start_time) AS month,
     count(*) FROM lesson WHERE EXTRACT(YEAR FROM start_time) = '2022' GROUP BY EXTRACT(month FROM start_time)
     ORDER BY EXTRACT(month FROM start_time) ASC;
@@ -35,7 +35,7 @@ SELECT
 
 -- siblings
 
-   SELECT student.person_id, COUNT(CASE WHEN sibling.student_id IS NOT NULL THEN 'has_sibling' END) AS number_of_siblings from student LEFT JOIN sibling ON student.person_id = sibling.student_id GROUP BY student.person_id ORDER BY student.person_id;
+  SELECT student.person_id, COUNT(CASE WHEN sibling.student_id IS NOT NULL THEN 'has_sibling' END) AS number_of_siblings from student LEFT JOIN sibling ON student.person_id = sibling.student_id GROUP BY student.person_id ORDER BY student.person_id;
 
 
 
@@ -43,3 +43,22 @@ SELECT
 
   SELECT instructor_person_id, count(*) FROM lesson WHERE EXTRACT(YEAR FROM start_time) = '2022' AND EXTRACT(MONTH FROM start_time) = '12' GROUP BY instructor_person_id HAVING COUNT(*) > 0
     ORDER BY count(*) DESC;
+
+-- Show all ensembles in next week
+SELECT lesson_id, to_char(start_time, 'Day') as weekday, genre, start_time,
+CASE 
+WHEN  (max_of_students - 10) = 0 
+THEN 'full boked'
+
+WHEN  (max_of_students - 9) = 1 or (max_seats - boked) = 2 
+THEN '1-2 seats left!'
+
+WHEN  (max_of_students - 7) > 2
+THEN 'Seats left'
+
+END
+    FROM lesson RIGHT JOIN ensembles ON lesson.id = ensembles.lesson_id 
+    WHERE date_trunc('week', start_time) = date_trunc('week', now()) + interval '1 week' ORDER BY genre, weekday;
+
+
+
